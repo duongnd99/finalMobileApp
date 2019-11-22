e1a0a56c57b3298454acd89f3aea798bc659d0d4
khanh <khanhtq.b8084@st.usth.edu.vn>
2019-11-22 11:42:15 +0700

    changing color when clicking each mail

--- final_mobile_project/app/src/main/java/com/example/final_mobile_project/InboxFragment.java
+++ final_mobile_project/app/src/main/java/com/example/final_mobile_project/InboxFragment.java
@@ -1,10 +1,14 @@
 package com.example.final_mobile_project;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
@@ -20,14 +24,17 @@ import androidx.fragment.app.Fragment;
 import org.w3c.dom.Text;
 
 public class InboxFragment extends Fragment {
+    boolean isColor = true;
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ScrollView view = (ScrollView) inflater.inflate(R.layout.inbox_fragment, container, false);
+    public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final ScrollView view = (ScrollView) inflater.inflate(R.layout.inbox_fragment, container, false);
         LinearLayout inboxFragment = view.findViewById(R.id.inbox_fragment_layout);
 
+
         for(int i=1; i<=3; i++){
-            LinearLayout inboxRow = (LinearLayout)inflater.inflate(R.layout.inbox_row, container, false);
+            final LinearLayout inboxRow = (LinearLayout)inflater.inflate(R.layout.inbox_row, container, false);
 
             LinearLayout inboxRowInfoLayout = inboxRow.findViewById(R.id.inbox_row_info_layout);
             ImageView userImage = inboxRow.findViewById(R.id.user_image);
@@ -44,14 +51,36 @@ public class InboxFragment extends Fragment {
 
 
             inboxFragment.addView(inboxRow);
-            inboxRow.setClickable(true);
-            inboxRow.setOnClickListener(new View.OnClickListener() {
+//            inboxRow.setClickable(true);
+            inboxRow.setOnTouchListener(new View.OnTouchListener() {
                 @Override
-                public void onClick(View v) {
+                public boolean onTouch(View v, MotionEvent event) {
+                    // TODO Auto-generated method stub
+                    switch(event.getAction())
+                    {
+                        case MotionEvent.ACTION_DOWN:
+                            inboxRow.setBackgroundColor(Color.GRAY);
+                            break;
+                        case MotionEvent.ACTION_UP:
+                            //set color back to default
+                            inboxRow.setBackgroundColor(Color.WHITE);
+                            break;
+                    }
                     Intent i = new Intent(getActivity().getApplicationContext(), MessageDetail.class);
+                    i.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
                     startActivity(i);
+                    return true;
                 }
             });
+//            inboxRow.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//
+//                    Intent i = new Intent(getActivity().getApplicationContext(), MessageDetail.class);
+//                    startActivity(i);
+//                }
+//
+//            });
         }
 
 
